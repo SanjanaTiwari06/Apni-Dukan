7ebc6e3d4509a1d2542fb7bd0d44906f33a5e579
Sanjana <sanjanat0609@gmail.com>
2026-06-04 09:44:43 +0530

    Update frontend URL in backend

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://apni-dukan-rust.vercel.app/:"
+    "https://e-commerce-website-apni-dukan-2w7p.vercel.app/"
   ]
 }));
 
